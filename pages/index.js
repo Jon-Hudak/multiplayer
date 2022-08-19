ad038e13ec1766a8a21a8a923d35482fbf4504f2
Jon-Hudak <shigshagor@gmail.com>
2022-08-19 14:00:21 -0400

    fix: moved game page to index

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,86 @@
-import Head from 'next/head'
-import Image from 'next/image'
-//import styles from '../styles/Home.module.css'
+import { google } from "googleapis";
+import ButtonContainer from "../components/ButtonContainer.jsx";
+import GameList from "../components/GameBar";
+import { useState } from "react";
+import { AppCont } from "../components/Styles/AppCont.jsx";
+import GameBar from "../components/GameBar";
+import { GameBarSt } from "../components/Styles/GameBarSt.jsx";
+const FREE_INDEX = 0;
+const RACE_INDEX = 1;
+const TITLE_INDEX = 2;
+const FIRST_PLAYER_INDEX = 3;
 
+const HEADERS = 0;
+const FIRST_GAME_INDEX = 1;
 
-export default function Home() {
-  return (
-    <div>
-      <a href='game'> <div>game </div></a>
-    </div>
-  )
+export async function getServerSideProps({ query }) {
+  //Auth
+  const auth = await google.auth.getClient({
+    scopes: ["https://www.googleapis.com/auth/spreadsheets.readonly"],
+  });
+  const sheets = google.sheets({ version: "v4", auth });
+
+  //Query
+  const { id } = query;
+  const range = `Sheet1!1:50`; //TODO: Expand to entire spreadsheet
+
+  const response = await sheets.spreadsheets.values.get({
+    spreadsheetId: process.env.SHEET_ID,
+    range,
+  });
+  const values = response.data.values;
+
+  //Result
+
+  const title = values[2];
+  const players = {};
+  const gameTitles = [];
+
+  for (let i = FIRST_PLAYER_INDEX; i < values[HEADERS].length; i++) {
+    players[values[HEADERS][i]] = false;
+  }
+
+  //const gameObjects=objectify(values);
+
+  return {
+    props: {
+      values,
+      players,
+    },
+  };
 }
+
+export default function Game({ values, players }) {
+  const [filter, setFilter] = useState(players);
+
+  function renderGameList(value, filter) {
+    if (value[TITLE_INDEX] !== "Title") {
+      for (const p in filter) {
+        if (filter[p] === true) {
+          if (!value.includes(p)) {
+            return null;
+          }
+        }
+      }
+
+      return <GameBarSt key={value[TITLE_INDEX]}>{value[TITLE_INDEX]}</GameBarSt>;
+
+      
+    }
+  }
+  return (
+    <>
+      <h1>Games :)</h1>
+      <AppCont className="appContainer"> 
+        <ButtonContainer
+          players={players}
+          filter={filter}
+          setFilter={setFilter}
+        />
+        <GameList>{values.map((values) => renderGameList(values, filter))}</GameList>
+        
+      </AppCont>
+    </>
+  );
+}
+
