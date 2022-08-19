75fe06915eb0699031dbd610b267d56ab6862546
Jon-Hudak <106953064+Jon-Hudak@users.noreply.github.com>
2022-08-19 19:08:14 -0400

    Add files via upload

---
+++ _next/static/75bFzdjs0DFB8pf9e707v/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/329-0191b173f9dc3102.js","static/chunks/pages/index-6d7c5381bd89ba9b.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
